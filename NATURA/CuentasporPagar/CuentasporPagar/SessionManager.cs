1e55e3c7640d2b9bf895476141ddb553080b79b5
jesus <jesus.santamaria8@gmail.com>
2018-08-03 14:53:06 -0500

    Cambios

---
+++ NATURA/CuentasporPagar/CuentasporPagar/SessionManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CuentasporPagar
+{
+    public class SessionManager
+    {
+        /// <summary>
+        /// Sets the specified key.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="data">The data.</param>
+        public static void Set<T>(string key, T data)
+        {
+            HttpContext context = HttpContext.Current;
+            context.Session[key] = data;
+        }
+
+        /// <summary>
+        /// Gets the specified key.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        public static T Get<T>(string key)
+        {
+            HttpContext context = HttpContext.Current;
+            return (T)context.Session[key];
+        }
+
+        /// <summary>
+        /// Delete the specified key.
+        /// </summary>        
+        /// <param name="key">The key.</param>        
+        public static void Del(string key)
+        {
+            HttpContext context = HttpContext.Current;
+            context.Session.Remove(key);
+        }
+
+        /// <summary>
+        /// Delete all keys.
+        /// </summary>                
+        public static void DelAll()
+        {
+            HttpContext context = HttpContext.Current;
+            context.Session.RemoveAll();
+        }
+    }
+}
